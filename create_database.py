69b1adf20d78b97549312c565a33dbe573492f91
Sam Turay Jr <st3387@drexel.edu>
2024-08-21 21:43:18 -0400

    First Commit

---
+++ create_database.py
@@ -0,0 +1,5 @@
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain.embeddings import OpenAIEmbeddings
+from  langchain.vectorstores.chroma import Chroma
